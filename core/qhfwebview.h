af76e9b5cd1b096ac8106d261c3e007c4c43eb79
XD.Yang <xd.yang@nf-3.com>
2015-09-19 23:02:20 +0800

    程序核心

---
+++ core/qhfwebview.h
@@ -0,0 +1,27 @@
+﻿#ifndef QHFWEBVIEW_H
+#define QHFWEBVIEW_H
+
+#include <QWidget>
+#include <QWebView>
+
+class QHFWebView : public QWebView
+{
+    Q_OBJECT
+public:
+    explicit QHFWebView(QWidget *parent = 0);
+
+    void     setTransparent( bool );
+    void     load(const QUrl& url);
+
+protected:
+//    void paintEvent(QPaintEvent *);
+    void mouseDoubleClickEvent(QMouseEvent *evt);
+signals:
+
+public slots:
+    void    registerObject();
+private:
+    QWidget  *m_parent;
+};
+
+#endif // QHFWEBVIEW_H
